d06073adf8b096cf730c0f356d2f22201199940c
Bobbie2 <bobbiedinh88@gmail.com>
2024-10-28 05:24:17 -0700

    created textfield

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder='Your course goal!'></TextInput>
+        <Button title='Add Your Goal'></Button>
+      </View>
+      <View>
+        <Text>List of goals....</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
+    gap: 30,
+    backgroundColor: 'orange',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  inputContainer: {
+    flexDirection: 'row',
+    gap: 10,
+    backgroundColor: '#cccccc',
+    width: '100',
+  },
+  textInput: {
+    flexGrow: 0,
+  }
 });
